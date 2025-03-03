97a0a7600e106272fb97dffb906dcf3b7e510fe3
Soulter <37870767+Soulter@users.noreply.github.com>
2025-03-04 00:38:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0", "repo url")
+@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0", "repo_url")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -16,3 +16,6 @@ class MyPlugin(Star):
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    async def terminate(self):
+        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
