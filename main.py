b25bc0640cbecb99b8c8ebf66ce49f09fc3769f2
Soulter <37870767+Soulter@users.noreply.github.com>
2025-03-04 11:14:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0", "repo_url")
+@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
