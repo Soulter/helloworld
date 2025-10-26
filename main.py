50715f08f40beabded56a8651ad9f9deb9890c47
Soulter <37870767+Soulter@users.noreply.github.com>
2025-10-26 22:30:47 +0800

    fix: formatting and comments in main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class MyPlugin(Star):
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
+
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("helloworld")
     async def helloworld(self, event: AstrMessageEvent):
